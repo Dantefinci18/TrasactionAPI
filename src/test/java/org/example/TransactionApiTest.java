ba831414629146bd335dee3b3c50377e60894ef4
Dantefinci18 <datalefi@gmail.com>
2026-06-15 01:17:47 -0300

    test: agrego test para que el response de PUT /transactions pueda tener un status OK

--- src/test/java/org/example/TransactionApiTest.java
+++ src/test/java/org/example/TransactionApiTest.java
@@ -2,14 +2,32 @@ package org.example;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TransactionApiController.class)
 public class TransactionApiTest {
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     @Disabled
-    public void realizarUnaTransaccionRespondeOK(){
+    public void realizarUnaTransaccionRespondeOK() throws Exception {
+        String body = """
+                { "amount": 5000,
+                "type": "cars"
+                }""";
 
+        mockMvc.perform(put("/transactions/10")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk());
     }
 
 }
